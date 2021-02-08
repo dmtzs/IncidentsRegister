09edd026c11d4c97144e6c04b91a83f72509d9f6
Diego Ms <diego.mtzs00@gmail.com>
2021-02-08 12:17:47 -0600

    Model for the incidents

--- Incidents/admin.py
+++ Incidents/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+#class CategoryAdmin(admin.ModelAdmin):
+    #readonly_fields= ("created at")
+
+#class IncidentAdmin(admin.ModelAdmin):
+    #readonly_fields= ("created_at". "updated_at")
+
+#admin.site.register(Category, CategoryAdmin)
+#admin.site.refister(Incident, IncidentAdmin)
--- Incidents/models.py
+++ Incidents/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import ckeditor.fields as ck
 
 # Create your models here.
+class RegIncident(models.Model):
+    CompleteDate= models.DateField(verbose_name= "Date")
+    Application= models.CharField(verbose_name= "Application Name", max_length= 30)
+    SealID= models.CharField(verbose_name= "SealID", max_length= 10)
+    Severity= models.CharField(verbose_name= "Severity", max_length= 4)
+    TicketNum= models.CharField(verbose_name= "Ticket", unique= True, max_length= 13)
+    StartTime= models.TimeField(verbose_name= "Start Hour")
+    EndTime= models.TimeField(verbose_name= "End Hour")
+    IssueDesc= ck.RichTextField(verbose_name= "Issue description")
+    ReportTo= models.CharField(verbose_name= "Report to regulator?", max_length= 3)
+    Impact= ck.RichTextField(verbose_name= "Impact")
+    Extras= models.FileField(verbose_name= "Another extras", upload_to= "ExtrasEv")
