389c06069b5cab06ad58c383e71ae51251d33e6d
Diego Ms <diego.mtzs00@gmail.com>
2021-02-11 16:10:19 -0600

    Update for the admin file of Incidents app

--- Incidents/admin.py
+++ Incidents/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from .models import RegIncident
 
 # Register your models here.
 #class CategoryAdmin(admin.ModelAdmin):
     #readonly_fields= ("created at")
 
-#class IncidentAdmin(admin.ModelAdmin):
-    #readonly_fields= ("created_at". "updated_at")
+class IncidentAdmin(admin.ModelAdmin):
+    readonly_fields= ("created_at", "updated_at")
 
 #admin.site.register(Category, CategoryAdmin)
-#admin.site.refister(Incident, IncidentAdmin)
+admin.site.refister(RegIncident, IncidentAdmin)
